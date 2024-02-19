34ba61454a2973bb4a089d23bf6df593eb597725
Lucas Barros <lucaslb5656@gmail.com>
2024-02-19 17:54:43 -0300

    dev

--- calendar.js
+++ calendar.js
@@ -5,23 +5,22 @@ let datainicial = new Date(anoAtual, mesAtual, 1)
 let datafinal = new Date(anoAtual, mesAtual+1, 0) 
 let calendar = []
 
-function loadCalendar(inicio, fim) {
+function setCalendar(inicio, fim) {
     if ((inicio instanceof Date) && (fim instanceof Date)) {
        datainicial = inicio
        datafinal = fim
     }
     for (let data = datainicial; data <= datafinal; data.setDate(data.getDate() + 1)) {
         dia = {
-            "data": data,
+            "data": new Date(data),
             "dia": data.getDate(),
-            "nomeDia": data.getDay()
+            "diaSemana": data.getDay(),
+            "eventos": []
         }
-        console.log(dia)
-        
+        calendar.push(dia) 
     }
-
 }
 
-loadCalendar()
+
 
 
