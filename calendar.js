9ffacfb72461798c0d1d6060ac1a2f7f825f4073
Lucas Barros <lucaslb5656@gmail.com>
2024-02-20 18:04:20 -0300

    sgreg

--- calendar.js
+++ calendar.js
@@ -1,26 +1,50 @@
-let hoje = new Date()
-let mesAtual = hoje.getMonth()
-let anoAtual = hoje.getFullYear()
-let datainicial = new Date(anoAtual, mesAtual, 1)
-let datafinal = new Date(anoAtual, mesAtual+1, 0) 
-let calendar = []
+let today = new Date()
+let initialDate = new Date(today.getFullYear(), today.getMonth(), 1)
+let finalDate = new Date(today.getFullYear(), today.getMonth()+1, 0) 
+let daysWeek = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"]
 
-function setCalendar(inicio, fim) {
-    if ((inicio instanceof Date) && (fim instanceof Date)) {
-       datainicial = inicio
-       datafinal = fim
+function loadCalendar(start, end) {
+    if (start instanceof Datetime && end instanceof Datetime){
+        initialDate = start
+        finalDate = end
     }
-    for (let data = datainicial; data <= datafinal; data.setDate(data.getDate() + 1)) {
-        dia = {
-            "data": new Date(data),
-            "dia": data.getDate(),
-            "diaSemana": data.getDay(),
-            "eventos": []
-        }
-        calendar.push(dia) 
-    }
+    let calendar = document.createElement('table')
+    header = setHeaderCalendar()
+    calendar.appendChild(header)
+
 }
 
+function setHeaderCalendar() {
+    let header = document.createElement('thead')
+    let row = document.createElement('tr')
+    let cell
+    for (let dayWeek of daysWeek){
+        cell = document.createElement('td')
+        cell.innerText = dayWeek
+        row.appendChild(cell)
+    }
+    header.appendChild(row)
+    return header
+}
 
+function setBodyCalendar() {
+    let data = initialDate;
+    let body = document.createElement('tbody')
+    while(data <= finalDate){
+        let row = document.createElement('tr')
+        let cell;
+        for (let dayWeek = 0; dayWeek < daysWeek.length; dayWeek++){
+            cell = document.createElement('td')
+            if (data.getDay() === dayWeek){
+                cell.innerText = data.getDate()
+                data.setDate(data.getDate()+1)
+            }
+            row.appendChild(cell)
+        }  
+        body.appendChild(row)
+    }
+    return body
+      
+}
 
-
+console.log(setBodyCalendar())
